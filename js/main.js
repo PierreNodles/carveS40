23feae2f6836fe6e04170750822d9efcede69db7
Pierre Djotni <pierre.djotni@gmail.com>
2019-01-24 19:04:12 +0100

    Slider product en place, no nav

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
 jQuery(function($) {
 
-  //functions
-  var position = $('h1').offset();
-  console.log(position);
 
 
 
@@ -39,21 +36,73 @@ jQuery(function($) {
   });
 
 
+  // SLIDER PRODUCT
+
+  var $slider = $('.slider_product'), // on cible le bloc du carrousel
+  $slide = $('.slider_product .product_slide'), // on cible les images contenues dans le carrousel
+  indexSlide = $slide.length - 1, // on définit l'index du dernier élément
+  i = 0, // on initialise un compteur
+  $currentSlide = $slide.eq(i); // enfin, on cible l'image courante, qui possède l'index i (0 pour l'instant)
+  $currentSlide.fadeIn(500)
+
+  function changeSlide(){
+    $currentSlide = $slide.eq(i); // enfin, on cible l'image courante, qui possède l'index i (0 pour l'instant)
+    $slide.fadeOut(500).delay(490); // on cache les images
+    $currentSlide.fadeIn(500); // on affiche seulement l'image courante
+  }
 
 
-// ANIMATION HORIZONTALE DU PARALLAX
 
-  // function animateBG() {
-  //     $('.parallax').animate({
-  //         'background-position': '-=50'
-  //     }, 120);
-  //     animateBG();
-  // }
-  //
-  // // $(window).scroll(function(){
-  //   animateBG();
-  // // })
 
+  changeSlide();
+
+
+  $slider.append('<div class="controls"> <div class="prev nav"><figure><img src="img/left-arrow.png"></figure><span>Nos autres longboards</span></div> <div class="next nav"><figure><img src="img/right-arrow.png"></figure><span>Nos autres longboards</span></div></div>');
+
+  $('.next').click(function(event) {
+    i++;
+    if (i > indexSlide) {
+      i = 0;
+    }
+    changeSlide();
+
+  });
+
+  $('.prev').click(function(event) {
+    i--;
+    if (i < 0) {
+      i = indexSlide;
+    }
+    changeSlide();
+
+  });
+
+
+  var loop;
+
+  function slider(){
+
+    loop = setTimeout(function(){
+
+      i++;
+      if (i > indexSlide) {
+        i = 0;
+      }
+      changeSlide();
+      slider();
+    }, 5000);
+
+  }
+
+  slider();
+
+  // On s'assure qu'en cas de nav manuelle, le chrono avant defilement revienne à 0;
+
+  $('.nav').click(function(event) {
+
+    clearTimeout(loop); // On sort de la boucle timeOut créée ans la function carrousel_1
+    slider(); // Puis on la relance
+  });
 
 
 
