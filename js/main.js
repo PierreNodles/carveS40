4f3999194838d0b55f3e1ebdccd11eda31d0e7c4
Pierre Djotni <pierre.djotni@gmail.com>
2019-01-28 03:46:58 +0100

    Desktop - OK

--- js/main.js
+++ js/main.js
@@ -1,5 +1,38 @@
 jQuery(function($) {
 
+  ////////////////////
+  ////////// SET HEIGHT
+  /////////////////////
+
+  // CARVE540
+
+  function setMainHeight() {
+    var $logoHeight = $('.logo').height();
+    var $sliderHeight = $('.carve540_slide').height();
+    var $totalHeight = $logoHeight + $sliderHeight;
+    $('.carve540_slider').height($totalHeight);
+
+  }
+
+  // BOUTIQUE PRODUIT
+  function setSliderHeight() {
+    var $slideHeight = $('.product_slide').height();
+    $('.slider_product .container').height($slideHeight);
+
+  }
+
+  setMainHeight();
+  setSliderHeight();
+
+  $(window).resize(function() {
+    setMainHeight() ;
+    setSliderHeight();
+  });
+
+
+  //////////////////////
+  /////////// JUMBOTRON
+  /////////////////////
 
 
 
@@ -29,6 +62,128 @@ jQuery(function($) {
 
 
 
+
+
+
+
+  ///////////////////
+  ////// CARVE 540
+  ///////////////////
+
+
+
+
+
+  ///////////// SLIDER CARVE 540
+
+  var $sliderCarve540 = $('.dp_carve540'), // on cible le bloc du slider
+  $slideCarve540 = $('.carve540_slide'), // on cible les slides contenues dans le slider
+  indexSlideCarve540 = $slideCarve540.length - 1, // on définit l'index du dernier élément
+  i540 = 0, // on initialise un compteur
+  $currentSlideCarve540 = $slideCarve540.eq(i540); // enfin, on cible la slide courante, qui possède l'index i (0 pour l'instant)
+  $currentSlideCarve540.fadeIn(500)
+
+  function changeSlideCarve540(direction){
+    $currentSlideCarve540 = $slideCarve540.eq(i540); // enfin, on cible l'image courante, qui possède l'index i (0 pour l'instant)
+    if (direction == "left" ) {
+      $slideCarve540.animate({opacity: 0, left: '-300px'}, 900).animate({left:'300px'}, 0).hide(0); // on cache les images
+    } else if (direction == "right" || !direction) {
+      $slideCarve540.animate({opacity: 0, left: '300px'}, 900).animate({left:'-300px'}, 0).hide(0); // on cache les images
+    }
+
+    $currentSlideCarve540.fadeIn(0).animate({opacity: 1.0, left :"0px", right :"0px"}, 900); // on affiche seulement l'image courante
+  }
+
+
+  changeSlideCarve540()
+
+
+  // AJOUT DES BOUTONS DE NAVIGATION
+  $('.dp_carve540 .controls_wrapper').append('<div class="controls"> <div class="prev_carve540 nav_carve540"><figure><img src="img/left-arrow.png"></figure></div> <div class="next_carve540 nav_carve540"><figure><img src="img/right-arrow.png"></figure></div></div>');
+
+  // GESTION DE LA NAVIGATION
+  $('.next_carve540').click(function(event) {
+    i540++;
+    if (i540 > indexSlide) {
+      i540 = 0;
+    }
+    changeSlideCarve540('right');
+
+  });
+
+  $('.prev_carve540').click(function(event) {
+    i540--;
+    if (i540 < 0) {
+      i540 = indexSlide;
+    }
+    changeSlideCarve540('left');
+
+  });
+
+
+  // LANCEMENT DU DEFILEMENT AUTOMATIQUE DU SLIDER_PRODUCT
+  var loopCarve540;
+  var popup_open = false;
+
+  function sliderCarve540(){
+
+    loopCarve540 = setTimeout(function(){
+
+      i540++;
+      if (i540 > indexSlideCarve540) {
+        i540 = 0;
+      }
+      if (popup_open == false) {
+      changeSlideCarve540();
+      sliderCarve540();
+      }
+    }, 5000);
+
+  }
+
+  sliderCarve540();
+
+  // On s'assure qu'en cas de nav manuelle, le chrono avant defilement revienne à 0;
+
+  $('.nav_carve540').click(function(event) {
+    clearTimeout(loopCarve540); // On sort de la boucle timeOut créée ans la function carrousel_1
+    sliderCarve540(); // Puis on la relance
+  });
+
+
+  ///////////// POP UP CARVE 540 FEATURES
+
+  /////////////////// FRONT
+
+  $('.board-front .trigger').click(function(event) {
+    $('.board-front .pop-up_features').fadeIn(700).css("display", "flex");
+    popup_open = true;
+  });
+
+  /////////////////// BACK
+
+  $('.board-back .trigger').click(function(event) {
+    $('.board-back .pop-up_features').fadeIn(700).css("display", "flex");
+    popup_open = true;
+
+  });
+
+  ////////////////// CLOSE BOTH
+
+  $('.carve540_pop-up_close').click(function(event) {
+    $('.pop-up_features').fadeOut(700);
+    clearTimeout(loopCarve540); // On sort de la boucle timeOut créée ans la function carrousel_1
+    popup_open = false;
+    sliderCarve540(); // Puis on la relance
+
+  });
+
+
+
+  ////////////////////////
+  //////////////// MOVIES
+  ///////////////////////
+
   // Display "Voir le film" au survol
   $('.videos .video').hover(function(){
     var $figcaption = $(this).find('figcaption');
@@ -56,12 +211,45 @@ jQuery(function($) {
   // Overlay sur l'élement non selectionné
 
 
+  ////////////////////////
+  // VIDEO POP UP YOUTUBE
+  ////////////////////////
+
+
+  $('.video').click(function(event) {
+    $('.pop-up_background').fadeIn(400);
+  });
+
+  $('.video_left').click(function(event) {
+    $('.pop-up_left').fadeIn({
+      duration:400,
+      queue: false
+    });
+  });
+
+  $('.video_right').click(function(event) {
+    $('.pop-up_right').fadeIn({
+      duration:400,
+      queue: false
+    })
+  });
+
+  $('.pop-up_close').click(function(event) {
+    $('.pop-up_background').fadeOut(400);
+    $('.pop-up').fadeOut();
+    var video = $(this).siblings(".pop-up_video").attr("src");
+    $(this).siblings(".pop-up_video").attr("src","");
+    $(this).siblings(".pop-up_video").attr("src",video);
+  });
 
 
 
 
-  // SLIDER PRODUCT
 
+
+  ////////////////////////
+  // SLIDER PRODUCT - BOUTIQUE
+  ////////////////////////
 
   var $slider = $('.slider_product'), // on cible le bloc du slider
   $slide = $('.slider_product .product_slide'), // on cible les slides contenues dans le slider
@@ -70,9 +258,14 @@ jQuery(function($) {
   $currentSlide = $slide.eq(i); // enfin, on cible la slide courante, qui possède l'index i (0 pour l'instant)
   $currentSlide.fadeIn(500)
 
-  function changeSlide(){
+  function changeSlide(direction){
     $currentSlide = $slide.eq(i); // enfin, on cible l'image courante, qui possède l'index i (0 pour l'instant)
-    $slide.animate({opacity: 0, left: '+300px'}, 900).animate({left:'-300px'}, 0); // on cache les images
+    if (direction == "left") {
+      $slide.animate({opacity: 0, left: '-300px'}, 900).animate({left:'300px'}, 0); // on cache les images
+    } else if (direction == "right" || !direction) {
+      $slide.animate({opacity: 0, left: '300px'}, 900).animate({left:'-300px'}, 0); // on cache les images
+    }
+
     $currentSlide.animate({opacity: 1.0, left :"0px", right :"0px"}, 900); // on affiche seulement l'image courante
 
     switch (i) {
@@ -106,7 +299,7 @@ jQuery(function($) {
     if (i > indexSlide) {
       i = 0;
     }
-    changeSlide();
+    changeSlide('right');
 
   });
 
@@ -115,7 +308,7 @@ jQuery(function($) {
     if (i < 0) {
       i = indexSlide;
     }
-    changeSlide();
+    changeSlide('left');
 
   });
 
@@ -155,7 +348,7 @@ jQuery(function($) {
   }
 
 
-    setSliderHeight();
+  setSliderHeight();
 
 
 
@@ -167,35 +360,6 @@ jQuery(function($) {
 
 
 
-
-  // VIDEO POP UP
-
-
-  $('.video').click(function(event) {
-    $('.pop-up_background').fadeIn(400);
-  });
-
-  $('.video_left').click(function(event) {
-    $('.pop-up_left').fadeIn({
-      duration:400,
-      queue: false
-    });
-  });
-
-  $('.video_right').click(function(event) {
-    $('.pop-up_right').fadeIn({
-      duration:400,
-      queue: false
-    })
-  });
-
-  $('.pop-up_close').click(function(event) {
-    $('.pop-up_background').fadeOut(400);
-    $('.pop-up').fadeOut();
-    var video = $(this).siblings(".pop-up_video").attr("src");
-    $(this).siblings(".pop-up_video").attr("src","");
-    $(this).siblings(".pop-up_video").attr("src",video);
-  });
 
 
 
